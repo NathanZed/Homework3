a45914dcd544c882f3f09590b0bd3aa1af2217ba
NathanZed <60272287+NathanZed@users.noreply.github.com>
2021-04-02 12:37:56 -0400

    Commit 2
    Imported https status codes, added use, get and delete.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-const express = require('express');
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import winston from 'winston';
 
 const app = express();
+app.use(bodyParser.json());
+
+// Winston
+const winstonLogger = winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+  ],
+});
+
+app.use((reg, res, next) => {
+  winstonLogger.info(reg.method);
+  next();
+});
 
 app.get('/', (req, res) => {
-  console.log('Got request');
   res.send('Hello World');
 });
 
+app.delete('/', (req, res) => {
+  res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
+});
+
 app.listen(8080);
-console.log('It works');
+winstonLogger.info('It connects to port 8080');
